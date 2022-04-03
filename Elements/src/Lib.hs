ac00c38e85197dcdfddf05838c95ddf087feef59
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2022-04-03 15:22:08 +0200

    Generate a simple model

--- Elements/src/Lib.hs
+++ Elements/src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import Graphics.Implicit
+import Graphics.Implicit.Definitions
+
+model = union [cube False (pure 20), translate (pure 20) $ sphere 15]
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  putStrLn "Generating model.stl"
+  writeSTL 1 "model.stl" model
