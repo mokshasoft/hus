b9035a65b18b7a65a79cc56b6bc0e5f47cba30db
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2022-04-03 18:21:09 +0200

    Test performance of a simple cube

--- Elements/src/Lib.hs
+++ Elements/src/Lib.hs
@@ -4,10 +4,12 @@ module Lib
 
 import Graphics.Implicit
 import Graphics.Implicit.Definitions
-
-model = union [cube False (pure 20), translate (pure 20) $ sphere 15]
+import System.TimeIt
 
 someFunc :: IO ()
 someFunc = do
-  putStrLn "Generating model.stl"
-  writeSTL 1 "model.stl" model
+  putStrLn "Generating model1.stl fast"
+  timeIt $ writeSTL 1 "model1.stl" $ cube False (V3 200 95 45)
+  putStrLn "Generating model2.stl slow"
+  timeIt $ writeSTL 1 "model2.stl" $ cube False (V3 4000 95 45)
+  putStrLn "Done"
