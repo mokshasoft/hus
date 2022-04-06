df8dab4c76f0ee73b1e3607f190e8fac8fc831ef
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2022-04-06 21:29:59 +0200

    Add the rest of the framed walls

--- Materials/Beams.hs
+++ Materials/Beams.hs
@@ -32,7 +32,7 @@ frame w h =
     , area = w * h
     , beamLength =
         h * fromInteger (1 + ceiling (w / ccx)) +
-        w * fromInteger (ceiling (h / ccy))
+        w * fromInteger (1 + ceiling (h / ccy))
     }
 
 frame' :: Float -> Float -> Float -> Material
@@ -44,6 +44,21 @@ bedroom = frame' 3.0 4.2 4.8
 
 bathroom :: Material
 bathroom = frame' 2.7 3.5 4.0 <> frame' 2.7 3.5 4.0 <> frame 2.5 4.0
+
+loadBearingWall :: Material
+loadBearingWall = frame 7500 4200
+
+innerCeiling :: Material
+innerCeiling = frame 7500 3000
+
+gardenRoof :: Material
+gardenRoof = frame 3750 2500
+
+garageInnerRoof :: Material
+garageInnerRoof = frame 6000 5000
+
+garageWall :: Material
+garageWall = frame 22000 2800
 
 -- | Run material calculation
 main :: IO ()
