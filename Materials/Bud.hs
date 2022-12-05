c7f62dc27dce734437f5feb6fc669bc506eebf75
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2022-12-05 22:21:59 +0100

    Page2

--- Materials/Bud.hs
+++ Materials/Bud.hs
@@ -44,7 +44,44 @@ page1 =
   , I "pressenning" 3247 JC ""
   ]
 
-items = page1
+page2 :: [Item]
+page2 =
+  [ I "thor" 6259 JC ""
+  , I "ecococon 1/2" 158575 JC ""
+  , I "värmandsschakt" 34015 JAD ""
+  , I "bygghandel" 1076 JC ""
+  , I "taisto" 1800 JC ""
+  , I "isolering Bauhaus 810kg" 11166 JAD ""
+  , I "isolering woodfibre" 5600 JC ""
+  , I "fuktmätare" 1609 JC ""
+  , I "vinschar" 1050 JC ""
+  , I "lis 16" 3000 JC ""
+  , I "ecococon #2" 160000 JAD ""
+  , I "ecococon #2" 850 JC ""
+  , I "såg" 70000 JAD ""
+  , I "såg" 70000 JC ""
+  , I "biltema" 934 JC ""
+  , I "alex v17" 1500 JC ""
+  , I "tak" 16250 JAD ""
+  , I "tak förskott" 1500 JAD ""
+  , I "edsvalla trä" 37592 JAD "15/5"
+  , I "värmlandschakt" 10350 JAD "15/5"
+  , I "bitförlängare" 347 JC ""
+  , I "alex 2:a v" 700 JC ""
+  , I "diesel" 491 JC ""
+  , I "indianwood" 51250 JC ""
+  , I "dewalt" 7739 JC ""
+  , I "ales 3:dje v" 700 JC ""
+  , I "karmskruv" 490 JAD "1/6"
+  , I "k-rauta" 2937 JAD ""
+  , I "verktygsbälte" 798 JAD ""
+  , I "ecococon #3" 136973 JC ""
+  , I "bits" 286 JC ""
+  , I "alex 4:de v" 500 JC ""
+  ]
+
+
+items = page1 ++ page2
 
 total :: [Item] -> Int
 total = sum . map cost
