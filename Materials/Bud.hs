128b720fb6e261a3c1c3660755de21bde6afc364
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2022-12-05 22:04:21 +0100

    add page1

--- Materials/Bud.hs
+++ Materials/Bud.hs
@@ -12,12 +12,39 @@ data Item =
     }
   deriving (Show)
 
-items :: [Item]
-items =
-  [ I "" 1 JC ""
-  , I "" 2 JC ""
-  , I "" 4 JAD ""
+page1 :: [Item]
+page1 =
+  [ I "4 fönster" 9500 JAD ""
+  , I "3 dörrar" 900 JAD ""
+  , I "byggekologibok" 1118 JAD ""
+  , I "förhandsbesked" 5007 JC ""
+  , I "gjutningsgrejor" 4264 JC ""
+  , I "betong" 2573 JC ""
+  , I "betong" 2573 JC ""
+  , I "bensin elverk" 430 JC ""
+  , I "betong" 581 JC ""
+  , I "transport sågverk" 3750 JC ""
+  , I "betong & grävning" 14424 JC ""
+  , I "fönster" 5600 JC ""
+  , I "15000 hos Johan i material" 15000 JAD ""
+  , I "bygglov" 15337 JAD ""
+  , I "BNA verktyg" 5270 JC ""
+  , I "motorsåg" 697 JC ""
+  , I "tegel 1/2" 4500 JAD ""
+  , I "ytterdörr" 4400 JAD ""
+  , I "tegel" 2700 JC ""
+  , I "Håkan" 6150 JAD ""
+  , I "tegel 1/2" 4500 JAD ""
+  , I "diesel" 550 JC ""
+  , I "BNA kablar" 1870 JC ""
+  , I "ove" 2000 JC ""
+  , I "thor" 2600 JC ""
+  , I "byggmax" 845 JC ""
+  , I "stuvbutiken" 1256 JC ""
+  , I "pressenning" 3247 JC ""
   ]
+
+items = page1
 
 total :: [Item] -> Int
 total = sum . map cost
