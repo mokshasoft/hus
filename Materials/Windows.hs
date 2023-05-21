419f6c3cfca602e52e437159e37f620876bcb8ff
Jonas Claesson <Jonas.Claesson@arbetsformedlingen.se>
2023-05-21 09:39:39 +0200

    Calculate sil lengths

--- Materials/Windows.hs
+++ Materials/Windows.hs
@@ -7,25 +7,34 @@ data Window =
     }
   deriving (Show)
 
+doors :: [Window]
+doors
+    -- North
+ =
+  [ W 913 2115 -- W02
+    -- South
+  , W 908 2010 -- W03
+    -- East
+  , W 1013 2115 -- W01
+    -- West
+  , W 908 2010 -- W03
+  ]
+
 windows :: [Window]
 windows
     -- North
  =
-  [ W 913 2115 -- W02
-  , W 1308 1210 -- W05
+  [ W 1308 1210 -- W05
   , W 1308 610 -- W08
   , W 1308 610 -- W08
     -- South
   , W 908 2010 -- W03
-  , W 908 2010 -- W03
   , W 558 1040 -- W07
     -- East
   , W 558 1050 -- W06
-  , W 1013 2115 -- W01
     -- West
   , W 1308 610 -- W08
   , W 908 610 -- W09
-  , W 908 2010 -- W03
   , W 1808 1210 -- W04
   ]
 
@@ -44,14 +53,18 @@ silLength = mmToM . sum . map width
 printSil :: IO ()
 printSil = do
   putStrLn "-- Fönsterbrädor --"
-  putStrLn $ "längd: " ++ (show $ silLength windows) ++ " m"
-  mapM_ (putStrLn . show . mmToM . width) windows
+  putStrLn $ "längd: " ++ (show $ silLength silOpenings) ++ " m"
+  mapM_
+    ((\s -> putStrLn $ s ++ " mm") . show . (\w -> (width w) - 5))
+    silOpenings
   putStrLn "\n"
+  where
+    silOpenings = windows ++ [last doors]
 
 printInsulation :: IO ()
 printInsulation = do
   putStrLn "-- Drev          --"
-  putStrLn $ "längd: " ++ (show $ insulationLength windows) ++ " m\n"
+  putStrLn $ "längd: " ++ (show $ insulationLength (windows ++ doors)) ++ " m\n"
 
 -- | Run material calculation
 main :: IO ()
